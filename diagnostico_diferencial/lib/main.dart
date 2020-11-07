298ba7bfe1f3f3136364db4e2b48ff71192fcb89
Nicolas Valderrama <nivalderramas@unal.edu.co>
2020-11-07 13:26:45 -0500

    GUI modificada

--- diagnostico_diferencial/lib/main.dart
+++ diagnostico_diferencial/lib/main.dart
@@ -1,39 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+
+  int mainBlue = 0xff048AEC;
+  int mainRed = 0xffED201C;
+  int softBlue = 0xffE7F3FC;
+
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Diagnostico Diferencial"),
+          title: Text(""),
         ),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              title: Text('Floating app bar'),
-              // Allows the user to reveal the app bar if they begin scrolling back
-              // up the list of items.
-              floating: true,
-              // Display a placeholder widget to visualize the shrinking size.
-              flexibleSpace: Placeholder(),
-              // Make the initial height of the SliverAppBar larger than normal.
-              expandedHeight: 750,
-            ),
-            SliverList(
-              // Use a delegate to build items as they're scrolled on screen.
-              delegate: SliverChildBuilderDelegate(
-                // The builder function returns a ListTile with a title that
-                // displays the index of the current item.
-                    (context, index) => ListTile(title: Text('Item #$index')),
-                // Builds 1000 ListTiles
-                childCount: 20,
+        backgroundColor: Color(softBlue),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child:Text(
+                "DIAGNÓSTICO DIFERENCIAL",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(mainBlue),
+                  height: 5,
+                ),
               ),
-            )
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 150, 0, 0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: RaisedButton(
+                        onPressed: (){},
+                        child:Text(
+                          "SÍNTOMAS",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        color: Color(mainBlue),
+                      ),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: (){},
+                        child:Text(
+                          "SIGNOS",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        color: Color(mainBlue),
+                      ),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: (){},
+                        child:Text(
+                          "DIAGNÓSTICO",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        color: Color(mainRed),
+                      ),
+                    ),
+                  ],
+              ),
+            ),
+
           ],
-        ),
+        )
       ),
     );
   }
