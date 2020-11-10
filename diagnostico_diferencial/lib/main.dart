076c7917302d41f795363e9aade8de668b50a942
Nicolas Valderrama <nivalderramas@unal.edu.co>
2020-11-10 16:35:09 -0500

    avance

--- diagnostico_diferencial/lib/main.dart
+++ diagnostico_diferencial/lib/main.dart
@@ -4,85 +4,117 @@ import 'package:flutter/widgets.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
 
+class MyApp extends StatelessWidget {
   int mainBlue = 0xff048AEC;
   int mainRed = 0xffED201C;
   int softBlue = 0xffE7F3FC;
+  double buttonsHeight = 50;
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(""),
-        ),
-        backgroundColor: Color(softBlue),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              child:Text(
-                "DIAGNÓSTICO DIFERENCIAL",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color(mainBlue),
-                  height: 5,
+          appBar: AppBar(
+            title: Text("Diagnóstico diferencial"),
+            centerTitle: true,
+          ),
+          backgroundColor: Color(softBlue),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.blue),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("Perfil"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.medical_services),
+                  title: Text("Diagnosticar"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.pending_actions_rounded),
+                  title: Text("Historial de diagnosticos"),
+                ),
+              ],
+            ),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                alignment: Alignment(20, 30),
+                child: Text.rich(
+                  TextSpan(
+                      text: "Bienvenido, ",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: "Usuario ",
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        )
+                      ]),
+                  style: TextStyle(fontSize: 50),
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 150, 0, 0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: RaisedButton(
-                        onPressed: (){},
-                        child:Text(
-                          "SÍNTOMAS",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 100.0,
+                    height: buttonsHeight,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "SÍNTOMAS",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-                        color: Color(mainBlue),
                       ),
+                      color: Color(mainBlue),
                     ),
-                    Container(
-                      child: RaisedButton(
-                        onPressed: (){},
-                        child:Text(
-                          "SIGNOS",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  ),
+                  ButtonTheme(
+                    minWidth: 100.0,
+                    height: buttonsHeight,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "SIGNOS",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-                        color: Color(mainBlue),
                       ),
+                      color: Color(mainBlue),
                     ),
-                    Container(
-                      child: RaisedButton(
-                        onPressed: (){},
-                        child:Text(
-                          "DIAGNÓSTICO",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        color: Color(mainRed),
-                      ),
-                    ),
-                  ],
+                  ),
+                ],
               ),
-            ),
-
-          ],
-        )
-      ),
+              ButtonTheme(
+                minWidth: 150.0,
+                height: 80,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "DIAGNÓSTICO",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Color(mainRed),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
